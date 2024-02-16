4c0b92bdf65ee2e00997ac076798b462d12c9ed0
Fonz13 <Fonz23>
2024-02-16 21:43:44 +0100

    render api

--- api/main.py
+++ api/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import search
 import requests
+from fastapi_utils.tasks import repeat_every
+
 
 # from config import settings
 
@@ -12,6 +14,17 @@ app = FastAPI()
 def root():
     return "Welcome to my name game API! use the /docs endpoint to see the docs"
 
+
+"""
+@app.on_event("startup")
+@repeat_every(seconds=60 * 10)  # 10 minute
+def call_self() -> None:
+    # Call self to keep the server awake.
+    # Server will go down after 15 minutes of inactivity.
+    x = requests.get(
+        "https://hm-search-api.onrender.com/test/", params={"test_str": "self-call"}
+    )
+"""
 
 app.include_router(search.router)
 
